cf9d8798bda46caab54384cbe9ba15ae051fd28c
HiraokaTakuya <hiraoka64@gmail.com>
2021-07-21 02:23:29 +0900

    Import Stockfish commit 9587eeeb5ed29f834d4f956b92e0e732877c47a7.

--- src/thread.rs
+++ src/thread.rs
@@ -1005,6 +1005,10 @@ impl Thread {
             {
                 let mut r = unsafe { (*self.reductions).get(improving, depth, move_count) };
 
+                if cut_node && depth <= Depth(10) && move_count <= 2 && !get_stack(stack, 0).in_check {
+                    r -= Depth::ONE_PLY;
+                }
+
                 if self.tt_hit_average > 473 * TT_HIT_AVERAGE_RESOLUTION * TT_HIT_AVERAGE_WINDOW / 1024 {
                     r -= Depth::ONE_PLY;
                 }
