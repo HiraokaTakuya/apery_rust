c2b78b51c092b7b34dc35fd14b656f57f577973b
HiraokaTakuya <hiraoka64@gmail.com>
2021-07-27 05:06:05 +0900

    Import Stockfish commit a88a38c3a91749181ffa5d6dc0af7314a70a1c41.

--- src/thread.rs
+++ src/thread.rs
@@ -1027,7 +1027,7 @@ impl Thread {
                 //    r -= Depth(2);
                 //}
 
-                if (root_node || !pv_node) && depth > Depth(10) && self.best_move_changes.load(Ordering::Relaxed) <= 2 {
+                if (root_node || !pv_node) && self.root_depth > Depth(10) && self.best_move_changes.load(Ordering::Relaxed) <= 2 {
                     r += Depth::ONE_PLY;
                 }
 
