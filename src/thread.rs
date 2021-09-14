6c80efc0d013976a27c95aed9ad6ab769d5a686b
HiraokaTakuya <hiraoka64@gmail.com>
2021-09-14 20:37:51 +0900

    Revert "Import Stockfish commit 31ebd918ea54b8c9fccbc1e1fa269772ed0949df."
    This reverts commit f4473c03c6db1d19a75433cc4f489bb2729c0a0e.

--- src/thread.rs
+++ src/thread.rs
@@ -890,6 +890,11 @@ impl Thread {
                     if !get_stack(stack, 0).in_check
                         && lmr_depth.0 < 7
                         && get_stack(stack, 0).static_eval.0 + 172 + 157 * lmr_depth.0 <= alpha.0
+                        && unsafe { (*cont_hists[0]).get(to, piece_moved_after_move) }
+                            + unsafe { (*cont_hists[1]).get(to, piece_moved_after_move) }
+                            + unsafe { (*cont_hists[3]).get(to, piece_moved_after_move) }
+                            + unsafe { (*cont_hists[5]).get(to, piece_moved_after_move) } / 3
+                            < 28255
                     {
                         continue;
                     }
