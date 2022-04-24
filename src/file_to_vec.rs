802c13c42a870be7aaaf3d0e458be7dd84fb4018
HiraokaTakuya <hiraoka64@gmail.com>
2022-04-25 02:45:29 +0900

    Fix a linting warning.

--- src/file_to_vec.rs
+++ src/file_to_vec.rs
@@ -1,3 +1,4 @@
+use anyhow::Result;
 use std::io::Read;
 
 #[allow(dead_code)]
@@ -10,25 +11,33 @@ pub fn as_u8_mut_slice<T>(p: &mut [T]) -> &mut [u8] {
     unsafe { std::slice::from_raw_parts_mut(p.as_mut_ptr() as *mut u8, std::mem::size_of::<T>() * p.len()) }
 }
 
-pub fn file_to_vec<P, T>(input_file: P) -> std::io::Result<Vec<T>>
+pub fn file_to_vec<P, T>(input_file: P) -> Result<Vec<T>>
 where
-    P: AsRef<std::path::Path>,
+    P: AsRef<std::path::Path> + std::fmt::Display,
     T: std::marker::Copy,
 {
     let mut f = std::fs::File::open(&input_file)?;
     let file_size = std::fs::metadata(&input_file)?.len() as usize;
-    let elements_len = file_size / std::mem::size_of::<T>();
-    // Don't use Vec<u8>. We need more strictly aligned data.
-    let mut v = Vec::<T>::with_capacity(elements_len);
-    unsafe {
-        v.set_len(elements_len);
+    if file_size % std::mem::size_of::<T>() != 0 {
+        anyhow::bail!(
+            "File size of {} must be a multiple of {}.",
+            input_file,
+            std::mem::size_of::<T>()
+        );
     }
-    let u8_slice = unsafe {
-        std::slice::from_raw_parts_mut(
-            v.as_mut_slice().as_mut_ptr() as *mut u8,
-            elements_len * std::mem::size_of::<T>(),
-        )
-    };
+    let elements = file_size / std::mem::size_of::<T>();
+    // Don't use Vec<u8>. We need more strictly aligned data.
+    let mut v = vec![std::mem::MaybeUninit::<T>::uninit(); elements];
+    let u8_slice = unsafe { std::slice::from_raw_parts_mut(v.as_mut_ptr() as *mut u8, elements * std::mem::size_of::<T>()) };
     f.read_exact(u8_slice)?;
+
+    // Vec<MaybeUninit<T>> to Vec<T>
+    let v = {
+        let mut v = std::mem::ManuallyDrop::new(v);
+        let p = v.as_mut_ptr() as *mut T;
+        let len = v.len();
+        let cap = v.capacity();
+        unsafe { Vec::from_raw_parts(p, len, cap) }
+    };
     Ok(v)
 }
